66c4d08a1b552a53037c45ec4d7811c5023fc3c0
Igor Lubimov <mr_july@mail.ru>
2013-10-02 13:04:35 +0200

    initial implementation

---
+++ init.php
@@ -0,0 +1,125 @@
+<?php
+require_once (__DIR__ . '/phpHypher/hypher.php');
+
+class Af_Hypher extends Plugin implements IHandler
+{
+    private $host;
+
+    function about()
+    {
+        return array(
+            1.0,   // version
+            'Insert soft hyphenation symbols into words for better typography.',   // description
+            'mr_july',   // author
+            false,   // is_system
+        );
+    }
+
+    function api_version()
+    {
+        return 2;
+    }
+
+    function init($host)
+    {
+        $this->host = $host;
+
+        $host->add_hook($host::HOOK_PREFS_TABS, $this);
+# only allowed for system plugins:        $host->add_handler('pref-hypher', '*', $this);
+        $host->add_hook($host::HOOK_ARTICLE_FILTER, $this);
+    }
+
+    function csrf_ignore($method)
+    {
+        $csrf_ignored = array("index", "edit");
+        return array_search($method, $csrf_ignored) !== false;
+    }
+
+    function before($method)
+    {
+        if ($_SESSION["uid"]) {
+            return true;
+        }
+        return false;
+    }
+
+    function after()
+    {
+        return true;
+    }
+
+    function hook_article_filter($article)
+    {
+        global $fetch_last_content_type;
+
+        $json_conf = $this->host->get($this, 'json_conf');
+        $owner_uid = $article['owner_uid'];
+        $data = json_decode($json_conf, true);
+
+        if (FALSE && !is_array($data)) {
+            // no valid JSON or no configuration at all
+            return $article;
+        }
+
+        $hypher = new phpHypher(__DIR__ . '/phpHypher/conf/hyph_ru_RU.conf');
+
+        $article['content'] = $hypher->hyphenate($article['content'], 'UTF-8', '­');
+
+        return $article;
+    }
+
+    function hook_prefs_tabs($args)
+    {
+        print '<div id="hypherConfigTab" dojoType="dijit.layout.ContentPane"
+            href="backend.php?op=af_hypher"
+            title="' . __('Hypher') . '"></div>';
+    }
+
+    function index()
+    {
+        $pluginhost = PluginHost::getInstance();
+        $json_conf = $pluginhost->get($this, 'json_conf');
+
+        print "<form dojoType=\"dijit.form.Form\">";
+
+        print "<script type=\"dojo/method\" event=\"onSubmit\" args=\"evt\">
+            evt.preventDefault();
+            if (this.validate()) {
+                new Ajax.Request('backend.php', {
+                    parameters: dojo.objectToQuery(this.getValues()),
+                    onComplete: function(transport) {
+                        if (transport.responseText.indexOf('error')>=0) notify_error(transport.responseText);
+                            else notify_info(transport.responseText);
+                    }
+                });
+                //this.reset();
+            }
+            </script>";
+
+        print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"op\" value=\"pluginhandler\">";
+        print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"method\" value=\"save\">";
+        print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"plugin\" value=\"af_hypher\">";
+
+        print "<table width='100%'><tr><td>";
+        print "<textarea dojoType=\"dijit.form.SimpleTextarea\" name=\"json_conf\" style=\"font-size: 12px; width: 99%; height: 500px;\">$json_conf</textarea>";
+        print "</td></tr></table>";
+
+        print "<p><button dojoType=\"dijit.form.Button\" type=\"submit\">".__("Save")."</button>";
+
+        print "</form>";
+    }
+
+    function save()
+    {
+        $json_conf = $_POST['json_conf'];
+
+        if (is_null(json_decode($json_conf))) {
+            echo __("error: Invalid JSON!");
+            return false;
+        }
+
+        $this->host->set($this, 'json_conf', $json_conf);
+        echo __("Configuration saved.");
+    }
+
+}
